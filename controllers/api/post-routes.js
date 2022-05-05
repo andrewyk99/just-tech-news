a5268ed2cb6c486d36991e03d80dca6d714b3ce3
andrewyk99 <andrewyk99@hotmail.com>
2022-05-05 02:08:14 -0600

    created single post page

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -94,13 +94,16 @@ router.post('/', (req, res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-    // Custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-        .then(updatedPostData => res.json(updatedPostData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+    // Make sure the session exists first
+    if (req.session) {
+        // Custom static method created in models/Post.js
+        Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+            .then(updatedPostData => res.json(updatedPostData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
 });
 
 // Update a Post's title
